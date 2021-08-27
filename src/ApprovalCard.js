04e38e3b7a052d3dbfea1d8f3f184304ec1854b7
UntuckedShirt <75454794+UntuckedShirt@users.noreply.github.com>
2021-08-27 18:32:52 -0400

    approval card update

---
+++ src/ApprovalCard.js
@@ -0,0 +1,22 @@
+// every function receives a first arugment of props
+
+
+import React from 'react';
+
+
+const ApprovalCard = (props) => {
+    console.log(props.children)
+    return (
+        <div className="ui card">
+            <div className="content">{props.children}</div>
+                <div className="extra content"></div>
+                    <div className="ui two buttons"></div>
+                <div className="ui basic green button">Approve</div>
+            <div className="ui basic red button">Reject</div>
+        </div>
+
+    );
+
+};
+
+export default ApprovalCard
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 // ./ in import means to look at the folder we are in and import from file
 // components are treated as jsx tags
+// communicate info from child component by taking theclosing tag of ApprovalCard
+// and want it to show a common detail, cut it and put on other side of comment detail
+
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 //The app is the parent component and the details within app are children or child componenets
 //Props or (properties) pass data from parent to chilld. Works like DNA
 //A child cannot pass data to the parent
@@ -12,13 +16,46 @@ import CommentDetail from './CommentDetail'
 const App = () => {
     return (
         <div className="ui container comments">
-    {/*parent component*/}   {/*prop}*/}          {/*prop value*/}                {/*refrencing a JS varaible use curlys*/}
-            <CommentDetail author="Nate" timeAgo="Today at 4:45pm" posts="Hey there" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Jate" timeAgo="Ysterday at 5:25pm" posts="Pleasure is all mine" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Sate" timeAgo="Yesterday at 3:25pm" posts="Feeling stressed" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Kate" timeAgo="Today at at 7:56pm" posts="Going camping tommorow" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Plate" timeAgo="Today at 9:33pm" posts="Hey pilgrim" avatar={faker.image.avatar()}/>
+            <ApprovalCard>
+            {/*parent component*/}     {/*refrencing a JS varaible use curlys*/}
+                <CommentDetail
+    {/*prop->*/}     author="Nate"
+                    timeAgo="Today at 4:45pm"
+                    posts="Hey there"
+                    avatar={faker.image.avatar()} /> {/*<-prop value*/}  
+            </ApprovalCard>
             
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jate"
+                    timeAgo="Ysterday at 5:25pm"
+                    posts="Pleasure is all mine"
+                    avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sate"
+                    timeAgo="Yesterday at 3:25pm"
+                    posts="Feeling stressed"
+                    avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail
+                    author="Kate"
+                    timeAgo="Today at at 7:56pm"
+                    posts="Going camping tommorow"
+                    avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail
+                    author="Plate"
+                    timeAgo="Today at 9:33pm"
+                    posts="Hey pilgrim"
+                    avatar={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     );
 
