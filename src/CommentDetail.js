24e2b0498a2a8771e97b727ab7cc37228729ce71
UntuckedShirt <75454794+UntuckedShirt@users.noreply.github.com>
2021-08-27 15:32:53 -0400

    Passing and receiving props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -3,16 +3,20 @@
 import React from 'react';
 import faker from 'faker';
 
-
-const CommentDetail = () => {
+//CommentDetail is lookin at props objet.
+//It sees author property
+//takes it and sticks it into an anchor props.author
+const CommentDetail = (props) => {
+    console.log(props)
     return (
+            /*(property) (value)*/
         <div className="comment">
                 <a href="/" className="avatar">
                     <img alt="avatar" src={faker.image.avatar()} />
                 </a>
                 <div className="content">
                     <a href="/" className="author">
-                    Nate
+                    {props.author}
                     </a>
                     <div className="metadata">
                         <span className="date"> Today at 8:00pm</span>
--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
+//The app is the parent component and the details within app are children or child componenets
+//Props or (properties) pass data from parent to chilld. Works like DNA
+//A child cannot pass data to the parent
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
+     {/*parent component*/}{/*prop}*/}
+            <CommentDetail author="Nate" />
+            <CommentDetail author="Jate"/>
+            <CommentDetail author="Sate"/>
+            <CommentDetail author="Kate"/>
+            <CommentDetail author="Plate"/>
             
         </div>
     );
