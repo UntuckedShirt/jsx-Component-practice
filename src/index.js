45856f658726874f8e0a1a794ffae666a8e1486e
UntuckedShirt <75454794+UntuckedShirt@users.noreply.github.com>
2021-08-26 21:21:48 -0400

    practicing components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+// ./ in import means to look at the folder we are in and import from file
+// components are treated as jsx tags
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+import CommentDetail from './CommentDetail'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail />
+            
+        </div>
+    );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
